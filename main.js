4a40b704284b58730d8c686de5a3260d3421d53f
Muhammad Isbah <muhammadisbah@yahoo.com>
2024-09-11 12:25:55 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env/node
+var skill = /** @class */ (function () {
+    function skill(elementId, buttonId) {
+        this.element = document.getElementById(elementId);
+        this.button = document.getElementById(buttonId);
+        this.addToggle();
+    }
+    skill.prototype.addToggle = function () {
+        var _this = this;
+        this.button.addEventListener('click', function () {
+            if (_this.element.style.display === 'none') {
+                _this.element.style.display = 'block';
+                _this.button.textContent = 'Hide Section';
+            }
+            else {
+                _this.element.style.display = 'none';
+                _this.button.textContent = 'Show Section';
+            }
+        });
+    };
+    return skill;
+}());
+console.log(skill);
